b29da39feb4d972db9faff99ed2f0e7b295919be
Freboy1 <dautovalisher33@gmail.com>
2024-10-23 16:33:16 +0500

    idk

---
+++ weather.js
@@ -0,0 +1,14 @@
+document.getElementById("getWeatherBtn").addEventListener("click", function() {
+    const city = "Astana";  
+    const apiKey = "a184cec28e18696aa72e9629c0f4f09c"; 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = data.weather[0].description;
+            const temp = data.main.temp;
+            document.getElementById("weatherDisplay").textContent = `Погода в ${city}: ${weather}, Температура: ${temp}°C`;
+        })
+        .catch(error => console.log("Error!!: ", error));
+});
