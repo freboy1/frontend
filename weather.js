e8a92cd97aa8a76002b5f5c2bd1999b960e5989b
Freboy1 <dautovalisher33@gmail.com>
2024-10-29 10:03:17 +0500

    Add dark mode for catalog

--- weather.js
+++ weather.js
@@ -1,4 +1,9 @@
-document.getElementById("getWeatherBtn").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current datetime
+    const now = new Date();
+    document.getElementById("datetime").textContent = now.toLocaleString();
+
+    // Fetch and display the weather
     const city = "Astana";  
     const apiKey = "a184cec28e18696aa72e9629c0f4f09c"; 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
